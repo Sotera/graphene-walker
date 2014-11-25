4d87ee025b6722fb9e2ca368b0252a6d63de8e3d
Andrew Weller <Andrew.Weller@soteradefense.com>
2014-11-25 09:24:50 -0500

    Changed add( SymbolConstants ) to override( SymbolConstants )

--- graphene-walker/graphene-walker-web/src/main/java/graphene/walker/web/services/DevelopmentModule.java
+++ graphene-walker/graphene-walker-web/src/main/java/graphene/walker/web/services/DevelopmentModule.java
@@ -15,12 +15,12 @@ public class DevelopmentModule
         // The factory default is true but during the early stages of an application
         // overriding to false is a good idea. In addition, this is often overridden
         // on the command line as -Dtapestry.production-mode=false
-        configuration.add(SymbolConstants.PRODUCTION_MODE, false);
+        configuration.override(SymbolConstants.PRODUCTION_MODE, false);
 
         // The application version number is incorprated into URLs for some
         // assets. Web browsers will cache assets because of the far future expires
         // header. If existing assets are changed, the version number should also
         // change, to force the browser to download new versions.
-        configuration.add(SymbolConstants.APPLICATION_VERSION, "4.0.8-SNAPSHOT-DEV");
+        configuration.override(SymbolConstants.APPLICATION_VERSION, "4.0.8-SNAPSHOT-DEV");
     }
 }
