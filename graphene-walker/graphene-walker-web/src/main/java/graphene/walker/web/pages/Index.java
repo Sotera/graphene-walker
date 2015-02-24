d8591b0901672190339cec27d52dbed91dfcef3d
djue <djue@phy6.net>
2015-02-24 12:58:46 -0500

    Updates to POM for 4.1.2 SNAPSHOT

--- graphene-walker/graphene-walker-web/src/main/java/graphene/walker/web/pages/Index.java
+++ graphene-walker/graphene-walker-web/src/main/java/graphene/walker/web/pages/Index.java
@@ -1,15 +1,11 @@
 package graphene.walker.web.pages;
 
-import graphene.model.idl.G_SymbolConstants;
 import graphene.model.idl.G_VisualType;
 import graphene.web.annotations.PluginPage;
 import graphene.web.pages.SimpleBasePage;
 
-import org.apache.tapestry5.SymbolConstants;
 import org.apache.tapestry5.alerts.AlertManager;
-import org.apache.tapestry5.annotations.Property;
 import org.apache.tapestry5.ioc.annotations.Inject;
-import org.apache.tapestry5.ioc.annotations.Symbol;
 import org.joda.time.DateTime;
 
 /**
@@ -17,14 +13,7 @@ import org.joda.time.DateTime;
  */
 @PluginPage(visualType = G_VisualType.TOP, menuName = "Walker Dashboard", icon = "fa fa-lg fa-fw fa-code-home")
 public class Index extends SimpleBasePage {
-	@Property
-	@Inject
-	@Symbol(SymbolConstants.TAPESTRY_VERSION)
-	private String tapestryVersion;
-	@Property
-	@Inject
-	@Symbol(G_SymbolConstants.APPLICATION_NAME)
-	private String appName;
+
 	@Inject
 	private AlertManager alertManager;
 
